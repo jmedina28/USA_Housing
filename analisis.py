d84e8e9698b0071d8abde16495345e501dfe0512
jmedina28 <juanperezmedina67@gmail.com>
2022-05-07 16:22:11 +0200

    añado descripción y columnas

--- analisis.py
+++ analisis.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("USA_Housing.csv")
 print(df)
+
+print("Las columnas del dataset son :\n", df.columns)
+print("La descripción del dataset es la siguiente: \n", df.describe())
