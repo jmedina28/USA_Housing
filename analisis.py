9a85475bfaece619f2970f1f91afdb8ceb87cdf3
jmedina28 <juanperezmedina67@gmail.com>
2022-05-07 17:43:09 +0200

    añado df2

--- analisis.py
+++ analisis.py
@@ -9,12 +9,12 @@ print("Las columnas del dataset son :\n", df.columns)
 print("La descripción del dataset es la siguiente: \n", df.describe())
 
 # calcula la media , mediana, desviación estándar y el valor máximo y mínimo
-def analisis(var):    
-    print(f"La media de la columna {var} es: ", df[var].mean())
-    print(f"La mediana de la columna {var} es: ", df[var].median())
-    print(f"La desviación estándar de la columna {var} es: ", df[var].std())
-    print(f"El valor máximo de la columna {var} es: ", df[var].max())
-    print(f"El valor mínimo de la columna {var} es: ", df[var].min())
+def analisis(dataset,var):    
+    print(f"La media de la columna {var} es: ", dataset[var].mean())
+    print(f"La mediana de la columna {var} es: ", dataset[var].median())
+    print(f"La desviación estándar de la columna {var} es: ", dataset[var].std())
+    print(f"El valor máximo de la columna {var} es: ", dataset[var].max())
+    print(f"El valor mínimo de la columna {var} es: ", dataset[var].min())
     print("\n")
 # extraemos una lista que contiene las columnas que vamos a analizar
 lista = []
@@ -23,7 +23,7 @@ for i in df.columns:
         lista.append(i)
 # llamamos a analisis para cada columna
 for i in lista:
-    analisis(i)
+    analisis(df,i)
 # relacion entre precio e ingresos
 plt.figure(figsize=(10,10))
 plt.scatter(df['Price'], df['Avg. Area Income'])
@@ -44,7 +44,13 @@ plt.ylabel('Edad')
 plt.show()
 # este print devuelve la antiguedad de la casa con mayor precio
 print("La antigüedad de la casa con mayor precio es: ", df['Avg. Area House Age'][df['Price'].idxmax()])
-# cree un dataframe con las casas con antiguedad menor que 3
-df2 = df[df['Avg. Area House Age'] < 3]
-# halla el precio medio de las casas con antiguedad menor que 3
-print("El precio medio de las casas con edad menor que 5 es: ", df2['Price'].mean())
+# obtengo df2 que contiene casas con antiguedad inferior a 6 años
+df2 = df[df['Avg. Area House Age'] < 6]
+# halla el precio medio de las casas con antiguedad menor que 6
+lista2 = []
+for i in df2.columns:
+    if df2[i].dtype == np.int64 or df2[i].dtype == np.float64:
+        lista2.append(i)
+# llamamos a analisis para cada columna
+for i in lista2:
+    analisis(df2,i)
